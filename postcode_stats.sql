e9d355f6fffef06987656d46caf50a78512a3e66
cjr90 <chris.ryan.90@gmail.com>
2020-08-30 21:13:33 +0100

    Update and rename postcode_pageviews_recent.sql to postcode_stats.sql

---
+++ postcode_stats.sql
@@ -0,0 +1,11 @@
+## One row per user per day. Shows the postcode for each user_id for each day
+## Runs once per day, after users_extract is run
+
+INSERT postcode_stats
+
+SELECT
+CURRENT_TIMESTAMP() AS timestamp,
+id,
+postcode
+
+FROM users_extract
