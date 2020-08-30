815d59c648bdf8980495ce261b630f05be6aee11
cjr90 <chris.ryan.90@gmail.com>
2020-08-30 21:19:29 +0100

    Update and rename pageviews_stats.sql to stg_pageviews_stats.sql

--- stg_pageviews_stats.sql
+++ stg_pageviews_stats.sql
@@ -1,7 +1,7 @@
 ## One row per pageview per user. Historical view of all pageviews by every user.
 ## Runs once per hour after pageviews_extract runs. Appends all of that data to this table.
 
-INSERT pageviews_stats
+INSERT stg_pageviews_stats
 
 SELECT
 CURRENT_TIMESTAMP() AS timestamp,
