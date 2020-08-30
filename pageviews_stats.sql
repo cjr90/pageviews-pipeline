8d7534b5e9899e2073f98da4313aa8a573fddcc5
cjr90 <chris.ryan.90@gmail.com>
2020-08-30 21:16:06 +0100

    Update and rename postcode_pageviews_stats.sql to pageviews_stats.sql

---
+++ pageviews_stats.sql
@@ -0,0 +1,11 @@
+## One row per pageview per user. Historical view of all pageviews by every user.
+## Runs once per hour after pageviews_extract runs. Appends all of that data to this table.
+
+INSERT pageviews_stats
+
+SELECT
+CURRENT_TIMESTAMP() AS timestamp,
+user_id,
+page_url
+
+FROM pageviews_extract
