b9e457b3e074102127d7935c7c50e936f7c6e63e
cjr90 <chris.ryan.90@gmail.com>
2020-08-30 21:17:04 +0100

    Update postcode_stats.sql

--- postcode_stats.sql
+++ postcode_stats.sql
@@ -1,5 +1,5 @@
 ## One row per user per day. Shows the postcode for each user_id for each day
-## Runs once per day, after users_extract is run
+## Runs once per day, after users_extract is run. Appends all of that new data into this table
 
 INSERT postcode_stats
 
